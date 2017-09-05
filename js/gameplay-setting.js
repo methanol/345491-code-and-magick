5632715de22698d4cd7ec3decc7746a0bdd3c6c0
methanol <methanol.b@gmail.com>
2017-09-05 19:36:47 +0700

    задание 13 - в работе

---
+++ js/gameplay-setting.js
@@ -0,0 +1,24 @@
+'use strict';
+(function () {
+  var wizardWidth = 70;
+
+  window.gameplaySetting = {
+    fireballSize: 220,
+    wizardSpeed: 3,
+    getFireballSpeed: function (left) {
+      if (left) {
+        return 5;
+      }
+      return 2;
+    },
+    getWizardHeight: function () {
+      return 1.337 * wizardWidth;
+    },
+    getWizardX: function (width) {
+      return width * 0.5;
+    },
+    getWizardY: function (height) {
+      return height / 3;
+    }
+  };
+})(window);
