7fc9e056c2d6bdab11303629c7e2691add62b838
methanol <methanol.b@gmail.com>
2017-09-06 13:08:51 +0700

    задание 13 - исправления

--- js/magnificent-functions.js
+++ js/magnificent-functions.js
@@ -1,28 +1,26 @@
 'use strict';
 (function () {
+  var ESC_CODE = 27;
 
   window.magnificentFunctions = {
     countNumber: function (n) {
       return Math.floor(Math.random() * n);
     },
-    ESC_CODE: 27,
     ENTER_CODE: 13,
     COAT: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
     EYES: ['black', 'red', 'blue', 'yellow', 'green'],
     closePopup: function (item) {
       item.classList.add('hidden');
-      document.removeEventListener('keydown');
     },
     openPopup: function (item) {
       item.classList.remove('hidden');
-      document.addEventListener('keydown');
     },
     onPopupEscPress: function (evt, item, useName) {
       if (evt.target === useName) {
         evt.stopPropagation();
         return;
       } else {
-        if (evt.keyCode === window.magnificentFunctions.ESC_CODE) {
+        if (evt.keyCode === ESC_CODE) {
           window.magnificentFunctions.closePopup(item);
         }
       }
