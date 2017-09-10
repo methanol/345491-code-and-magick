711a6f6bde948888a11b4837070541d505ac3e29
methanol <methanol.b@gmail.com>
2017-09-11 00:32:53 +0700

    задание 19

--- js/wizards-creation.js
+++ js/wizards-creation.js
@@ -2,7 +2,6 @@
 (function () {
   var userSetup = document.querySelector('.setup');
   userSetup.classList.remove('hidden');
-  userSetup.querySelector('.setup-similar').classList.remove('hidden');
 
   var similarListElement = document.querySelector('.setup-similar-list');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
@@ -26,19 +25,41 @@
   function renderWizard(wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
     return wizardElement;
   }
 
   function addWizards(wiz) {
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < wiz.length; i++) {
+
+    for (var i = 0; i < 4; i++) {
       fragment.appendChild(renderWizard(wiz[i]));
     }
     similarListElement.appendChild(fragment);
+    userSetup.querySelector('.setup-similar').classList.remove('hidden');
   }
 
-  addWizards(createWizards());
+  function errorHandler(errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: orange;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '50px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  }
+
+  window.backend.load(addWizards, errorHandler);
+
+  var form = userSetup.querySelector('.setup-wizard-form');
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), function () {
+      userSetup.classList.add('hidden');
+    }, errorHandler);
+    evt.preventDefault();
+  });
 
 })();
