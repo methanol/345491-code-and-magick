8fa5eee79446ba70f8c4a4477ff70175c92648c0
methanol <methanol.b@gmail.com>
2017-09-11 00:41:06 +0700

    задание 19 - удалил ненужную функцию

--- js/wizards-creation.js
+++ js/wizards-creation.js
@@ -6,22 +6,6 @@
   var similarListElement = document.querySelector('.setup-similar-list');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-  function createWizards() {
-    var FIRST_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-    var SECOND_NAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-    var wizards = [];
-
-    for (var i = 0; i < 4; i++) {
-      wizards[i] =
-        {
-          name: FIRST_NAME[window.magnificentFunctions.countNumber(8)] + ' ' + SECOND_NAME[window.magnificentFunctions.countNumber(8)],
-          coatColor: window.magnificentFunctions.COAT[window.magnificentFunctions.countNumber(6)],
-          eyesColor: window.magnificentFunctions.EYES[window.magnificentFunctions.countNumber(5)]
-        };
-    }
-    return wizards;
-  }
-
   function renderWizard(wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
