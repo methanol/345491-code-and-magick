7832db14367182a4cb3c9ccf00bb9f8237bd553d
methanol <methanol.b@gmail.com>
2017-09-12 23:20:25 +0700

    задание 21 - исправление функции

--- js/wizards-creation.js
+++ js/wizards-creation.js
@@ -88,15 +88,7 @@
   }
 
   window.wizardsCreation = {
-    updateWizards: function () {
-      addWizards(wizards.sort(function (left, right) {
-        var rankDiff = getRank(right) - getRank(left);
-        if (rankDiff === 0) {
-          rankDiff = namesComparator(left.name, right.name);
-        }
-        return rankDiff;
-      }));
-    }
+    updateWizards: updateWizards
   };
 
 
